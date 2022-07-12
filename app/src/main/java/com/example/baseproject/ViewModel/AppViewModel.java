51a98f81c3ffdba66a6d16349ef0446d13c8b9b1
ChungNguyen <85283125+ndchung892@users.noreply.github.com>
2022-07-12 23:14:47 +0700

    a

--- app/src/main/java/com/example/baseproject/ViewModel/AppViewModel.java
+++ app/src/main/java/com/example/baseproject/ViewModel/AppViewModel.java
@@ -99,7 +99,7 @@ public class AppViewModel extends ViewModel {
             throw new IllegalStateException("unEnabled constructor");
 
 
-            //asudhkjhwqkoijhekqwjheioquwhekljqwheiouhaksljdhoiqwue
+            //asudhkkuyhiohqerkjhqioumkjnbasfuioyqjkbreqwiohckjxbiuq
         }
     }
 }
